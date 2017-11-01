e3e4be72e618a703c8c5247462e699dc5f3678c3
Anne-Laure Fontaine <anne.lfontaine@gmail.com>
2017-11-01 22:20:38 +0100

    bibliotheque.sql is more weightless

--- files/bibliotheque.sql
+++ files/bibliotheque.sql
@@ -1,26 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Mer 01 Novembre 2017 à 21:55
--- Version du serveur :  5.7.20-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `oz`
---
-
--- --------------------------------------------------------
 
 --
 -- Structure de la table `bibliotheque`
@@ -34,25 +12,8 @@ CREATE TABLE `bibliotheque` (
   `genre` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `bibliotheque`
---
 ALTER TABLE `bibliotheque`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `bibliotheque`
---
 ALTER TABLE `bibliotheque`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
